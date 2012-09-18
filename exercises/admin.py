3820062407f1b48977316225ad4361377027c775
alexbruckner <bruckner.alex@gmail.com>
2012-09-18 19:01:47 +0100

    model admin search filter additions

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,26 +1,51 @@
 from exercises.models import Lecturer, Student, Mark, Submission, Exercise, Course, Subscription
 from django.contrib import admin
 
-admin.site.register(Lecturer)
-admin.site.register(Student)
-admin.site.register(Mark)
-admin.site.register(Submission)
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'firstname', 'lastname', 'email', 'created')
+        list_filter = ('username', 'firstname', 'lastname', 'created')
+        ordering = ('username',)
+        search_fields = ('username','firstname','lastname')
+
+admin.site.register(Lecturer, UserAdmin)
+admin.site.register(Student, UserAdmin)
+
+class MarkAdmin(admin.ModelAdmin):
+	list_display = ('submission', 'lecturer', 'mark', 'created')
+        list_filter = ('submission__exercise__title', 'lecturer__username', 'mark', 'created')
+        ordering = ('-created',)
+        search_fields = ('lecturer__username', 'mark')
+
+admin.site.register(Mark, MarkAdmin)
+
+class SubmissionAdmin(admin.ModelAdmin):
+	list_display = ('exercise', 'student', 'created')
+        list_filter = ('exercise__title', 'student__username', 'created')
+        ordering = ('-created',)
+
+admin.site.register(Submission, SubmissionAdmin)
 
 class ExerciseAdmin(admin.ModelAdmin):
 	list_display = ('title', 'course')
-        list_filter = ('title', 'course')
+        list_filter = ('title', 'course__title')
         ordering = ('-created',)
         search_fields = ('title',)
-
 
 admin.site.register(Exercise, ExerciseAdmin)
 
 class CourseAdmin(admin.ModelAdmin):
 	list_display = ('title', 'lecturer', 'created')
-        list_filter = ('title', 'lecturer')
+        list_filter = ('title', 'lecturer__username' ,'created')
         ordering = ('-created',)
         search_fields = ('title',)
 
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Subscription)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+	list_display = ('course', 'student', 'created')
+        list_filter = ('course__title', 'student__username' ,'created')
+        ordering = ('-created',)
+        search_fields = ('course__title','student__username')
+
+admin.site.register(Subscription, SubscriptionAdmin)
 
