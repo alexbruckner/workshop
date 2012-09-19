e6dde4dc56ea8a6d40355f730c7113cd92e06435
Alex Bruckner <alex.bruckner@semantico.com>
2012-09-19 14:57:29 +0100

    redirecting / to /workshop/home

--- urls.py
+++ urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^workshop/', include('exercises.urls')),
+   	url(r'^$', redirect_to, {'url': '/workshop/home'}),
+	url(r'^workshop/', include('exercises.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+	# Uncomment the next line to enable the admin:
+    	url(r'^admin/', include(admin.site.urls)),
 )
