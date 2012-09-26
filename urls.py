0b46a03535fd18ad51eaa553fb44befb471308da
alexbruckner <bruckner.alex@gmail.com>
2012-09-26 22:06:46 +0100

    fixed static files

--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
-from django.conf import settings
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,4 +18,5 @@ urlpatterns = patterns('',
 
 	# Uncomment the next line to enable the admin:
     	url(r'^admin/', include(admin.site.urls)),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + staticfiles_urlpatterns() 
+
