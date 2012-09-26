2f244243dfff319af1890c1479faad3b81b0aa66
alexbruckner <bruckner.alex@gmail.com>
2012-09-26 21:40:17 +0100

    static css attempt

--- urls.py
+++ urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -17,4 +19,4 @@ urlpatterns = patterns('',
 
 	# Uncomment the next line to enable the admin:
     	url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
